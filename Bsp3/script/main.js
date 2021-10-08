a4bbe4a82f7987c2b4c8b0aa175c0a45a9613161
Luisa <massimo.e.luisa.au@gmail.com>
2021-10-08 13:52:11 +0200

    ultima versione su github

--- Bsp3/script/main.js
+++ Bsp3/script/main.js
@@ -7,6 +7,7 @@
 
 
 //leggo label
+let i=1;
 console.log(document.getElementById("todoFeld").Value)
 document.getElementById("create").addEventListener("click", function(){
     let todoInput = document.getElementById("todoFeld").value;
@@ -16,12 +17,13 @@ document.getElementById("create").addEventListener("click", function(){
     
     const bu = document.createElement("button"); 
     bu.innerHTML="Delete";
+    bu.className=i;
     bu.id="deleteTodoElement";
     console.log("Hallo");
     li.appendChild(bu);
 
     document.querySelector("#cards ul").appendChild(li);
- 
+    i++;
 });
 
 document.querySelector("#cards ul").addEventListener("click", calcelElementOfList);
@@ -31,16 +33,10 @@ function calcelElementOfList(e){
     console.log("cancella elemento");
 //	let c = e.target.innerText;
     let idElement = e.target.id;
-    console.log(idElement);
+    console.log(idElement +  "  element e il valore della i e` : "+i+" la classe é: "+ e.target.className);
+    let list = document.querySelectorAll("#cards ul li");
 
-    document.querySelector("#cards ul").removeChild(e.target);
-    /*
-	for (let el of e.target.parentElement.children) {
-		if (el.innerText === c){
-			el.classList.toggle("highlight");		
-		}
-	};
-    */
+    e.target.remove();
 }
 
 //let result = responseMessage2.filter(item => item.Color==farbe);
