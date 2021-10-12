e9a9aaa7620ca7ff5be693535ddb3d8fff0fc379
Luisa <massimo.e.luisa.au@gmail.com>
2021-10-12 09:17:45 +0200

    fetch

--- Bsp3/script/main.js
+++ Bsp3/script/main.js
@@ -1,17 +1,13 @@
 "use strict";
 
-// hier kommen ihre Skripte hin
-
-
-
-//leggo label
 let i=1;
 console.log(document.getElementById("todoFeld").Value)
 document.getElementById("create").addEventListener("click", function(){
     let todoInput = document.getElementById("todoFeld").value;
 
     const li = document.createElement("li");
-    li.innerHTML=todoInput;
+    li.innerHTML=i+" "+todoInput;
+    li.className=i;
     
     const bu = document.createElement("button"); 
     bu.innerHTML="Delete";
@@ -19,9 +15,8 @@ document.getElementById("create").addEventListener("click", function(){
     bu.id="deleteTodoElement";
     li.appendChild(bu);
 
-
     const bu2 = document.createElement("button"); 
-    bu2.innerHTML="Over";
+    bu2.innerHTML="Up";
     bu2.className=i;
     bu2.id="OverTodoElement";
     li.appendChild(bu2);
@@ -35,8 +30,6 @@ document.getElementById("create").addEventListener("click", function(){
     document.querySelector("#cards ul").appendChild(li);
     i++;
 
-
-    window.onload;
 });
 
 document.querySelector("#cards ul").addEventListener("click", calcelElementOfList);
@@ -45,94 +38,49 @@ document.querySelector("#cards ul").addEventListener("click", calcelElementOfLis
 function calcelElementOfList(e){
     if(e.target.innerHTML=="Delete")  {
         console.log("delete element");
-        let idElement = e.target.id;
-        console.log(idElement +  "  element e il valore della i e` : "+i+" la classe é: "+ e.target.className);
+        console.log(e.target.id +  "  element e il valore della i e` : "+i+" la classe é: "+ e.target.className);
         let listHtml = document.querySelectorAll("#cards ul li");
         let list=Array.from(listHtml);
         let elementToRemove = list[Number(e.target.className)-1];
-        document.querySelector("#cards ul").removeChild(elementToRemove); 
-    }  
-    else if(e.target.innerHTML=="Over")  {
-        console.log("move over ");
-        let idElement = e.target.id;
-        console.log(idElement +  "  element e il valore della i e` : "+i+" la classe é: "+ e.target.className);
-        let listHtml = document.querySelectorAll("#cards ul li");
-        let list=Array.from(listHtml);
-        list=[1,2,3,4,5,6,7,8,9,0];
 
-        console.log("Array prima : "+list);
-      
-    }
-    window.onload;
-}
+        //document.querySelector("#cards ul").removeChild(elementToRemove); 
+        document.querySelector("#cards ul").removeChild(e.target.parentElement); 
+        /*
+        for (let el of e.target.parentElement.parentElement.children) {
+            if (el.className ==Number(e.target.className)-1){
+                console.log("el	"+el);	
+                console.log("e.target : "+e.target);
+            }
+        };
+        */
 
-
-/*
-function moveUp(){
-    for (var i = 0; i < upLink.length; i++) {
-        upLink[i].addEventListener('click', function () {
-            var wrapper = this.parentElement;
-
-            if (wrapper.previousElementSibling)
-                wrapper.parentNode.insertBefore(wrapper, wrapper.previousElementSibling);
-        });
+    }   else if(e.target.innerHTML=="Up")  {
+        /*console.log("move over ");
+        console.log(e.target.id +  "  element e il valore della i e` : "+i+" la classe é: "+ e.target.className);
+       
+        for (let el of e.target.parentElement.parentElement.children) {
+                console.log(el.className);       
+                 console.log(el.innerHTML);
+        };*/
+        moveUp(e.target.parentNode);
+    }   else if(e.target.innerHTML=="Down")  {
+        moveDown(e.target.parentNode);       
     }
 }
 
-function moveUp(){
-	for (var i = 0; i < downLink.length; i++) {
-		downLink[i].addEventListener('click', function () {
-			var wrapper = this.parentElement;
-
-			if (wrapper.nextElementSibling)
-			    wrapper.parentNode.insertBefore(wrapper.nextElementSibling, wrapper);
-		});
-	}
-
-}
-*/
 
 function moveUp(element) {
-    console.log("moveUP");
+    //console.log("moveUP, element id:  ");
+    //console.log(element.innerHTML);
+    
     if(element.previousElementSibling)
       element.parentNode.insertBefore(element, element.previousElementSibling);
   }
   function moveDown(element) {
-    console.log("moveDOWN");
+    //console.log("moveDOWN");
     if(element.nextElementSibling)
       element.parentNode.insertBefore(element.nextElementSibling, element);
   }
-  //document.querySelector('ul').addEventListener('click', function(e) {
-    document.querySelectorAll("#OverTodoElement").addEventListener('click', function(e) {
-    if(e.target.id === 'DownTodoElement') moveDown(e.target.parentNode);
-    else if(e.target.id === 'OverTodoElement') moveUp(e.target.parentNode);
-  });
 
 
-/*
- function move () {
-	var upLink = document.querySelectorAll("#OverTodoElement");
 
-	for (var i = 0; i < upLink.length; i++) {
-		upLink[i].addEventListener('click', function () {
-            console.log("LLLLLLL Over");
-			var wrapper = this.parentElement;
-
-			if (wrapper.previousElementSibling)
-			    wrapper.parentNode.insertBefore(wrapper, wrapper.previousElementSibling);
-		});
-	}
-
-	var downLink = document.querySelectorAll("#DownTodoElement");
-
-	for (var i = 0; i < downLink.length; i++) {
-		downLink[i].addEventListener('click', function () {
-            console.log("LLLLLLL  Down");
-			var wrapper = this.parentElement;
-
-			if (wrapper.nextElementSibling)
-			    wrapper.parentNode.insertBefore(wrapper.nextElementSibling, wrapper);
-		});
-	}
-}
-*/
