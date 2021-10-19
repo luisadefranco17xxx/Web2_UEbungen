f1a417b1b1242a433da4293fc1b658059a86a8ad
Luisa <massimo.e.luisa.au@gmail.com>
2021-10-19 19:09:03 +0200

    absolute pos lesen

--- Bsp3/script/main3.js
+++ Bsp3/script/main3.js
@@ -5,23 +5,24 @@
 document.querySelector(".move").addEventListener("click", moveQuadrat);
 
 function moveQuadrat(e){
-    console.log("qui");
-    if(e.target.id=="Links"){
-        console.log("links");    
+    if(e.target.id=="Links"){    
         let obj=document.getElementById('IdBox');
-        let pos=obj.style.left;
-        let newPos=" ";
-        for(let i=0;i<pos.length-2; i++){
-            newPos[i]=pos[i];
-        }
-        window.alert(pos);
-        window.alert(newPos);
-        obj.style.left=25+"px";
-        //e.target.style.left=200 +"px";
-        //obj.style.left=-5+"px"+Number(obj.style.left) +"px";
-        obj.style.color='red';
-        //docobjument.style.position=position-10;
-        //window.alert("l posizione é:  "+obj.style.left);
-        //window.alert(Number(obj.style.left));
+        var objRectangle = obj.getBoundingClientRect()
+        obj.style.left=objRectangle.left-25+"px";
+    }
+    if(e.target.id=="Recht"){    
+        let obj=document.getElementById('IdBox');
+        var objRectangle = obj.getBoundingClientRect()
+        obj.style.left=objRectangle.left+25+"px";
+    }
+    if(e.target.id=="Hoch"){    
+        let obj=document.getElementById('IdBox');
+        var objRectangle = obj.getBoundingClientRect()
+        obj.style.top=objRectangle.top-25+"px";
+    }
+    if(e.target.id=="Runter"){    
+        let obj=document.getElementById('IdBox');
+        var objRectangle = obj.getBoundingClientRect()
+        obj.style.top=objRectangle.top+25+"px";
     }
 }
